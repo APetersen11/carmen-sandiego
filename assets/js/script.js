7b0284419e2ef91bc32bf3991cd4eb7ab35a8821
Alyssa Petersen <aptrsn11@gmail.com>
2022-08-18 11:58:14 -0400

    dynamically create html elements without API context, update CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,18 +17,44 @@ fetch(
     console.log(data);
   });
 
-var fiveDayCard = document.querySelector(".five-day-cards");
+// Dynamically create one day card
+let oneDayCard = document.createElement("div");
+//do I need to do this? Can I just use the 5 day cards? wait for Lawren
 
 // Dynamically Create Elements
 //1. Create the element
-let card = document.createElement("div");
-let cardH4 = document.createElement("h4");
+//let card = document.createElement("div");
+//let cardH4 = document.createElement("h4");
 // 2. give element content
-cardH4.textContent = "8.19.22";
+//cardH4.textContent = "8.19.22";
 
 // Optional - add any attributes
-card.setAttribute("class", "card");
+//card.setAttribute("class", "card");
 
 // 3. Append to the page
-card.append(cardH4);
+//card.append(cardH4);
+//fiveDayCard.append(card);
+
+var fiveDayCard = document.querySelector(".five-day-cards");
+
+//Dynamically create 5 day cards
+let card = document.createElement("div");
+let cardDate = document.createElement("h4");
+let cardTemp = document.createElement("p");
+let cardWind = document.createElement("p");
+let cardHumidity = document.createElement("p");
+
+//REPLACE WITH API FILLED//
+cardDate.textContent = "8.19.22";
+//add image
+cardTemp.textContent = "99";
+cardWind.textContent = "15 mph";
+cardHumidity.textContent = "20%";
+
+card.setAttribute("class", "card");
+
+card.append(cardDate);
+card.append(cardTemp);
+card.append(cardWind);
+card.append(cardHumidity);
 fiveDayCard.append(card);
